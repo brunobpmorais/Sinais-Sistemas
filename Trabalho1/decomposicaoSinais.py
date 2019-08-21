5c4390fd4af776c431b30dbaec20deae3500303a
QYZM (Bruno Prado Morais) <qyzm@novonordisk.com>
2019-08-21 14:33:59 -0300

    funcoes

--- Trabalho1/decomposicaoSinais.py
+++ Trabalho1/decomposicaoSinais.py
@@ -2,35 +2,44 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def EvenSignal (funcao):
-    flp = np.flip(funcao)
-    evenpt = (funcao+flp)/2
+
+def PartePar (entfcn):
+    flp = np.flip(entfcn)
+    evenpt = (entfcn+flp)/2
     return evenpt
 
-def OddSignal (funcao):
-    flp = np.flip(funcao)
-    oddpt = (funcao-flp)/2
+def ParteImpar (entfcni):
+    flp = np.flip(entfcni)
+    oddpt = (entfcni-flp)/2
     return oddpt
 
-
-#funcoes
-x = np.arange(0, 8)
-funcao = 2*x**2
-print(x)
-print(funcao)
-evenpt = EvenSignal()
-
-plt.title('Sinal')
-plt.subplot(412)
-plt.stem(x, funcao)
-plt.title('Parte Par')
-plt.subplot(413)
-plt.stem(x, EvenSignal())
-plt.title('Parte Ímpar')
-plt.subplot(414)
-plt.stem(x, OddSignal())
-plt.show()
+def Plotar (t, f, ptp, pti):
+    plt.title('Sinal')
+    plt.subplot(412)
+    plt.stem(t, f)
+    plt.title('Parte Par')
+    plt.subplot(413)
+    plt.stem(t, ptp)
+    plt.title('Parte Ímpar')
+    plt.subplot(414)
+    plt.stem(t, pti)
+    plt.show()
     
+ 
+tmp = np.arange(0,8)    
+funcao = tmp**2
+parpt = PartePar(funcao)
+imparpt = ParteImpar(funcao)
+#somapts = Soma()
+print ('Sinal: '+ funcao,'\n')
+print ('Parte par: '+ parpt,'\n')
+print ('Parte impar: '+ imparpt,'\n')
+#print ('Soma: ', somapts'\n')
+i = input('Deseja plotar os graficos? (Y/N)?')
+if (i=='Y'):
+    Plotar(tmp, funcao, parpt, imparpt)
+
+
 
 #oddSignal = 1/2*(array1[n]-array1[])
 
