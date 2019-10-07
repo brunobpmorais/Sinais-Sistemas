2201ad89006afe5ca1f0e62e468fc5a96af57c1b
brunobpmorais <brunobpmorais@gmail.com>
2019-10-07 18:10:46 -0300

    finalcommit

--- Trabalho1/decomposicaoSinais.py
+++ Trabalho1/decomposicaoSinais.py
@@ -15,38 +15,64 @@ def startSignal(signal, starttime):
     startsignal = np.arange(starttime, starttime+lensignal, 1)
     return startsignal
 
+#-x[-n]
+def reverseSignal(signal):
+    xr = np.flip(signal)
+    return xr
+
+#sinal incial reverso
+def reverseSignalStrt(signalbgn, signalend):
+    reversestrt = np.arange(-signalend, -signalbgn+1)
+    return reversestrt
+
+#intervalo de toda a função
+def genVector(startsignal, reversesignalstrt):
+    genvector = np.arange(min(min(startsignal), min(reversesignalstrt)), max(max(startsignal), max(reversesignalstrt))+1)
+    return genvector
+
+#preenchendo com zeros
+def zeroFill(signal, genvector, signalbgn, reversesignal):
+    dif = abs(len(signal)-len(genvector))
+    zeros = np.zeros(dif)
+    if vetorgeral[0]==signalbgn:
+        xrg = np.hstack((zeros, reversesignal))
+    else:
+        xrg = np.hstack((reversesignal, zeros))
+
+    return xrg   
+
+def flipZeros(xrg):
+    xg = np.flip(xrg)
+    return xg
+
+#calcular partes par e impar segundo formula
+def evenPt(xg, xrg):
+    even = 0.5*(xg+xrg)
+    return even
+
+def oddPt(xg, xrg):
+    odd= 0.5*(xg-xrg)
+    return odd
+
 #Definindo comprimento do sinal
 comprimento = 20 #comprimento do sinal de entrada
 tempoinicial = -3
 sinal = calcSignal(comprimento)
 sinalinicial = startSignal(sinal, tempoinicial)
-
-#-x[-n]
-xr = np.flip(sinal)
-sinalinicialR = np.arange(-fimsinal, -iniciosinal+1)
-
-#intervalo de toda a função
-vetorgeral = np.arange(min(min(sinalinicial), min(sinalinicialR)), max(max(sinalinicial), max(sinalinicialR))+1)
-lengeral = len(vetorgeral)
-
-#preenchendo com zeros
-dif = abs(lensinal-lengeral)
-zeros = np.zeros(dif)
-if vetorgeral[0]==iniciosinal:
-    xrg = np.hstack((zeros, xr))
-else:
-    xrg = np.hstack((xr, zeros))
-    
-xg = np.flip(xrg)
-
-#calcular partes par e impar segundo formula
-par = 0.5*(xg+xrg)
-impar= 0.5*(xg-xrg)
+iniciosinal = sinalinicial[0]
+fimsinal = sinalinicial[comprimento-1]
+xr = reverseSignal(sinal)
+sinalinicialR = reverseSignalStrt(iniciosinal, fimsinal)
+vetorgeral = genVector(sinalinicial, sinalinicialR)
+vetorzeros = zeroFill(sinal, vetorgeral, iniciosinal, xr)
+zeroreverso = flipZeros(vetorzeros)
+par = evenPt(zeroreverso, vetorzeros)
+impar = oddPt(zeroreverso, vetorzeros)
 
 #funcoes de plot 
 
 plt.title('Sinal')
-plt.stem(vetorgeral, xg)
+plt.stem(vetorgeral, zeroreverso)
 plt.show()
 
 plt.title('Parte Par')
